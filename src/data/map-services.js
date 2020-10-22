3fbe10ed39767d83ecefd79d422b88192c3f20dd
Alessandro Passarelli <71466131+Ale1x@users.noreply.github.com>
2020-10-22 15:50:53 +0200

    Update map-services.js

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -3,13 +3,13 @@ const accessToken = process.env.GATSBY_MAPBOX_KEY;
 export const mapServices = [
   {
     name: 'OpenStreetMap',
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributo,
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
   },
   {
     name: 'Mapbox',
     attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributo, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Immagini © <a href="https://www.mapbox.com/">Mapbox</a>',
     url: `https://api.mapbox.com/styles/v1/adamalston/ckdzcawm40b7919lo2k1qu91c/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`
   }
 ];
